b4ff2dd567e0b52687decf11d6cfe2af362a9b79
Mufid <mufidcode@gmail.com>
2019-10-06 13:27:30 +0700

    add 2.add_todo

--- App.js
+++ App.js
@@ -1,117 +1,123 @@
-import React, {Component} from 'react';
+import React, { Component } from "react"
+// import Icon from "react-native-vector-icons/Ionicons"
 import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  FlatList,
-  Button,
-} from 'react-native';
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    FlatList,
+    Button
+} from "react-native"
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      text: '',
-      tasks: [
-        {key: 'work', done: false},
-        {key: 'swim', done: false},
-        {key: 'study', done: false},
-        {key: 'sleep', done: false},
-        {key: 'run', done: false},
-      ],
-    };
-    this.handleOnClick = this.handleOnClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    //   this.setState({
-    //     text: e.target.value,
-    //   });
-    // alert(e);
-    this.setState({
-      text: e,
-    });
-  }
-
-  handleOnClick() {
-    // alert(this.state.text);
-    if (this.state.text !== '') {
-      const newTask = {key: this.state.text, done: false};
-      this.setState({
-        tasks: this.state.tasks.concat(newTask),
-        text: '',
-      });
+        this.state = {
+            text: "",
+            tasks: [
+                { key: "work", done: false },
+                { key: "swim", done: false },
+                { key: "study", done: false },
+                { key: "sleep", done: false },
+                { key: "run", done: false }
+            ]
+        }
+        this.handleOnClick = this.handleOnClick.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
-    // alert(this.state.tasks);
 
-    // alert('clicked');
-  }
+    handleChange(e) {
+        //   this.setState({
+        //     text: e.target.value,
+        //   });
+        // alert(e);
+        this.setState({
+            text: e
+        })
+    }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        {/*<View>
-           <TextInput
-            placeholder="Add some task"
-            style={styles.input}
-            value={this.state.text}
-            onChangeText={text => {
-              this.handleChange(text);
-            }}
-          />
-          <Button
-            style={styles.button}
-            onPress={this.handleOnClick}
-            title="add"
-          />
-        </View> */}
-        <FlatList
-          data={this.state.tasks}
-          renderItem={({item}) => (
-            <Text style={styles.item}>{item.key}</Text>
-          )}></FlatList>
-      </View>
-    );
-  }
+    handleOnClick() {
+        // alert(this.state.text);
+        if (this.state.text !== "") {
+            const newTask = { key: this.state.text, done: false }
+            this.setState({
+                tasks: this.state.tasks.concat(newTask),
+                text: ""
+            })
+        }
+        // alert(this.state.tasks);
+
+        // alert('clicked');
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <TextInput
+                        placeholder="Add some task"
+                        style={styles.input}
+                        value={this.state.text}
+                        onChangeText={text => {
+                            this.handleChange(text)
+                        }}
+                    />
+                    <Button
+                        style={styles.button}
+                        onPress={this.handleOnClick}
+                        title="add"
+                    />
+                </View>
+                <FlatList
+                    data={this.state.tasks}
+                    renderItem={({ item }) => (
+                        <View>
+                            <Text style={styles.item}>{item.key}</Text>
+                            {/* <Button title="del" /> */}
+                            {/* <Button title="update" /> */}
+                        </View>
+                    )}
+                ></FlatList>
+            </View>
+        )
+    }
 }
 
-export default App;
+export default App
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-    borderBottomWidth: 1,
-  },
-  button: {
-    height: 50,
-    marginBottom: 30,
-    width: 100,
-    alignItems: 'center',
-    backgroundColor: '#2196F3',
-  },
-  buttonText: {
-    textAlign: 'center',
-    padding: 20,
-    color: 'white',
-  },
-  input: {
-    height: 50,
-    width: 250,
-    backgroundColor: '#ffffff',
-    paddingLeft: 15,
-    paddingRight: 15,
-  },
-  inputContainer: {
-    height: 50,
-  },
-});
+    container: {
+        // flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+        borderBottomWidth: 1
+    },
+    button: {
+        height: 50,
+        marginBottom: 30,
+        width: 100,
+        alignItems: "center",
+        backgroundColor: "#2196F3"
+    },
+    buttonText: {
+        textAlign: "center",
+        padding: 20,
+        color: "white"
+    },
+    input: {
+        height: 50,
+        width: 250,
+        backgroundColor: "#ffffff",
+        paddingLeft: 15,
+        paddingRight: 15
+    },
+    inputContainer: {
+        height: 50
+    }
+})
